90abf60f43d5365ff346cf60333b21b26d21d01a
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-21 15:56:05 +0530

    [Priyanshu]modified MSTestMood Analyser

---
+++ MSTestMoodAnalyser/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestMoodAnalyser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestMoodAnalyser")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d0006ff-2cac-4bfe-b389-6d5156bccf3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MSTestMoodAnalyser/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MoodAnalyser;
+
+namespace MSTestMoodAnalyser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //UC1
+        [TestMethod]
+        public void Given_Happymood_Expecting_Happy_Results()
+        {
+            //Arrange
+            MoodToAnalyse mood = new MoodToAnalyse("I am in happy mood");
+            string expected = "happy";
+
+            //Act
+            string actual = mood.Analyser();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //UC2.1
+        [TestMethod]
+        public void Given_nullmood_Expecting_Exception_Result()
+        {
+            //Arrange
+            MoodToAnalyse mood = new MoodToAnalyse("happy");
+            string expected = "happy";
+
+            //Act
+            string actual = mood.Analyser();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //UC3.1-Null
+        [TestMethod]
+        public void Given_Emptymood_Using_CustomException_Return_Null()
+        {
+            //Arrange
+            MoodToAnalyse mood = new MoodToAnalyse(null);
+            string expected = "Mood should not be null";
+            try
+            {
+
+                //Act
+                string actual = mood.Analyser();
+            }
+            catch (MoodAnalyserException exception)
+            {
+                //Assert
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+
+        //UC3.2-Empty Mood
+        [TestMethod]
+        public void Given_Emptymood_Using_CustomException_Return_Empty()
+        {
+            //Arrange
+            MoodToAnalyse mood = new MoodToAnalyse("");
+            string expected = "Mood should not be empty";
+            try
+            {
+
+                //Act
+                string actual = mood.Analyser();
+            }
+            catch (MoodAnalyserException exception)
+            {
+                //Assert
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+
+    }
+}
